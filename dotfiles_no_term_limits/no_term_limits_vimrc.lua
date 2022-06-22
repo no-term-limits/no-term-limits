bd216f8b444ac1a3559aaf273b191331e8aac61f
jasquat <jasquat@users.noreply.github.com>
2022-06-22 15:43:49 -0400

    added filters for pyright to ignore variables starting with underscores and called kwargs w/ burnettk

--- dotfiles_no_term_limits/no_term_limits_vimrc.lua
+++ dotfiles_no_term_limits/no_term_limits_vimrc.lua
@@ -154,3 +154,47 @@ local null_ls_sources = {
 null_ls.setup({
   sources = null_ls_sources,
 })
+
+function filter(arr, func)
+	-- Filter in place
+	-- https://stackoverflow.com/questions/49709998/how-to-filter-a-lua-array-inplace
+	local new_index = 1
+	local size_orig = #arr
+	for old_index, v in ipairs(arr) do
+		if func(v, old_index) then
+			arr[new_index] = v
+			new_index = new_index + 1
+		end
+	end
+	for i = new_index, size_orig do arr[i] = nil end
+end
+
+
+function filter_diagnostics(diagnostic)
+	-- Only filter out Pyright stuff for now
+	if diagnostic.source ~= "Pyright" then
+		return true
+	end
+
+	-- Allow kwargs to be unused, sometimes you want many functions to take the
+	-- same arguments but you don't use all the arguments in all the functions,
+	-- so kwargs is used to suck up all the extras
+	if diagnostic.message == '"kwargs" is not accessed' then
+		return false
+	end
+
+	-- Allow variables starting with an underscore
+	if string.match(diagnostic.message, '"_.+" is not accessed') then
+		return false
+	end
+
+	return true
+end
+
+function custom_on_publish_diagnostics(a, params, client_id, c, config)
+	filter(params.diagnostics, filter_diagnostics)
+	vim.lsp.diagnostic.on_publish_diagnostics(a, params, client_id, c, config)
+end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	custom_on_publish_diagnostics, {})
