cfb227a268842a251286183c1822c53fb32aea0c
jasquat <jasquat@users.noreply.github.com>
2022-06-28 17:12:37 -0400

    do not auto format with tsserver w/ burnettk

--- dotfiles_no_term_limits/no_term_limits_vimrc.lua
+++ dotfiles_no_term_limits/no_term_limits_vimrc.lua
@@ -104,7 +104,9 @@ require("typescript").setup({
     disable_commands = false, -- prevent the plugin from creating Vim commands
     debug = false, -- enable debug logging for commands
     server = { -- pass options to lspconfig's setup method
-        on_attach = ...,
+        on_attach = function(client)
+        client.resolved_capabilities.document_formatting = false
+    end,
     },
 })
 
