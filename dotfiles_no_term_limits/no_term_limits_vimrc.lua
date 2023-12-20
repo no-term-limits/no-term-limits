32133f30d6149360bbf143f42aa06249b9b2387e
burnettk <burnettk@users.noreply.github.com>
2023-12-20 13:05:18 -0500

    add yaml language server and codegpt

--- dotfiles_no_term_limits/no_term_limits_vimrc.lua
+++ dotfiles_no_term_limits/no_term_limits_vimrc.lua
@@ -168,6 +168,12 @@ lspconfig['pyright'].setup {
   end)()
 }
 
+-- npm install -g yaml-language-server
+-- understands github workflow yamls by default
+-- you can add your own schemas at the top of files like this:
+-- https://github.com/sartography/spiff-arena/commit/13b153f021184ac68758d9cf4b694996a8838151
+lspconfig.yamlls.setup{}
+
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
@@ -498,3 +504,43 @@ vim.keymap.set("n", "i", function()
     return "i"
   end
 end, { expr = true, desc = "properly indent on empty line when insert" })
+
+
+require("codegpt.config")
+
+-- Override the default chat completions url, this is useful to override when testing custom commands
+-- vim.g["codegpt_chat_completions_url"] = "http://127.0.0.1:800/test"
+
+vim.g["codegpt_commands"] = {
+  ["tests"] = {
+    language_instructions = {
+        python = "Use the pytest testing framework. When writing tests for controllers, use the client (FlaskClient) fixture and make calls to the method like client.get(f'/v1.0/tasks/etc')",
+    },
+  },
+  ["doc"] = {
+    -- Language specific instructions for python filetype
+    language_instructions = {
+        python = "Use the Google style docstrings."
+    },
+
+    -- Overrides the max tokens to be 1024
+    max_tokens = 1024,
+  },
+  ["code_edit"] = {
+    -- Overrides the system message template
+    system_message_template = "You are {{language}} developer.",
+
+    -- Overrides the user message template
+    user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nEdit the above code. {{language_instructions}}",
+
+    -- Display the response in a popup window. The popup window filetype will be the filetype of the current buffer.
+    callback_type = "code_popup",
+  },
+  -- Custom command
+  ["modernize"] = {
+    user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nModernize the above code. Use current best practices. Only return the code snippet and comments. {{language_instructions}}",
+    language_instructions = {
+        cpp = "Use modern C++ syntax. Use auto where possible. Do not import std. Use trailing return type. Use the c++11, c++14, c++17, and c++20 standards where applicable.",
+    },
+  }
+}
