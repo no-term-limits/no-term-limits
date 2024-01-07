e463e6580b7f34ba674f6c269b744bfdb118642e
burnettk <burnettk@users.noreply.github.com>
2024-01-06 21:21:53 -0500

    add lazy extras

---
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/lazy-extras.lua
@@ -0,0 +1,10 @@
+-- this is an override of part of the default lazyvim lua/config/lazy.lua, since i'd rather not fully override the file with a symlink to our repo.
+-- the lazyvim extras (like lazyvim.plugins.extras.lang.typescript) actually do load like i intend. i hope it isn't double loading lazyvim.
+return {
+  { "LazyVim/LazyVim" },
+  -- import any extras modules here
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+}
