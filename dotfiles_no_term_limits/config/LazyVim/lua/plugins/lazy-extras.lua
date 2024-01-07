b6350b20914940f2e1940ae26dc2b72c6972cc67
burnettk <burnettk@users.noreply.github.com>
2024-01-06 23:44:08 -0500

    add copilot

--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/lazy-extras.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/lazy-extras.lua
@@ -6,5 +6,8 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.python" },
+
+  { import = "lazyvim.plugins.extras.coding.copilot" },
+
   -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 }
