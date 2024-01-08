3ffac6dc70d174e73664360584e95b259a100d02
burnettk <burnettk@users.noreply.github.com>
2024-01-08 11:18:43 -0500

    no clipboard syncing

--- dotfiles_no_term_limits/config/LazyVim/lua/config/options.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/config/options.lua
@@ -7,3 +7,6 @@ vim.opt.mouse = ""
 
 -- swapfiles are mega annoying
 vim.opt.swapfile = false.
+
+-- turn off unnamedplus clipboard, since we don't want the main register syncing with system clipboard
+vim.opt.clipboard = ""
