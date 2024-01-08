c168cba4bfcfd34fe25c2f509fd21879e0252a8a
burnettk <burnettk@users.noreply.github.com>
2024-01-08 17:01:17 -0500

    tell lua to use two spaces for indentation in this repo

--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/conform.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/conform.lua
@@ -1,11 +1,14 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters = {
-			-- use extra args with shfmt. -i 2 says use two spaces. otherwise it uses tabs
-			shfmt = {
-				prepend_args = { "-i", "2" },
-			},
-		},
-	},
+  "stevearc/conform.nvim",
+  opts = {
+    formatters = {
+      -- use extra args with shfmt. -i 2 says use two spaces. otherwise shfmt uses tabs.
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+      stylelua = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
 }
