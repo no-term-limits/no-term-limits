8409af64e9771de501908c204aa58e45df07c7d0
burnettk <burnettk@users.noreply.github.com>
2024-01-09 13:04:38 -0500

    add cp and new method for localleader ew

---
+++ dotfiles_no_term_limits/config/LazyVim/lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+vim.keymap.set(
+  "n",
+  "cp",
+  [[:let @+ = expand("%:p")<CR>]],
+  { desc = "copy file to clipboard", noremap = false, silent = false }
+)
+
+-- add "find [in] dir" keymap to find files in the directory of the current buffer with telescope
+-- replaces these keymaps:
+--   cnoremap %% <C-R>=expand('%:h').'/'<cr>
+--   map <localleader>ew :e %%
+local builtin = require("telescope.builtin")
+local utils = require("telescope.utils")
+vim.keymap.set("n", "<leader>fd", function()
+  builtin.find_files({ cwd = utils.buffer_dir() })
+end, { noremap = false, silent = false, desc = "Find Files (buffer dir)" })
