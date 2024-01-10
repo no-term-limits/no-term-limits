3376f35b13ce2a1ed8f4cbff82add39d30e5f6f4
burnettk <burnettk@users.noreply.github.com>
2024-01-09 22:21:44 -0500

    fix crazy annoying luasnip behavior where you jump back into the snippet when hitting tab

--- dotfiles_no_term_limits/config/LazyVim/lua/config/autocmds.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/config/autocmds.lua
@@ -5,3 +5,27 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "TelescopeSelection", { link = "TelescopeResultsDiffAdd", bg = "none" })
   end,
 })
+
+local luasnip = require("luasnip")
+
+-- if you are in the middle of a snippet and leave insert mode, we want to forget about the snippet forever
+-- https://github.com/L3MON4D3/LuaSnip/issues/656#issuecomment-1500869758
+-- https://github.com/L3MON4D3/LuaSnip/issues/258#issuecomment-1429989436
+vim.api.nvim_create_autocmd("ModeChanged", {
+  group = vim.api.nvim_create_augroup("UnlinkLuaSnipSnippetOnModeChange", {
+    clear = true,
+  }),
+  pattern = { "s:n", "i:*" },
+  desc = "Forget the current snippet when leaving the insert mode",
+  callback = function(evt)
+    -- If we have n active nodes, n - 1 will still remain after a `unlink_current()` call.
+    -- We unlink all of them by wrapping the calls in a loop.
+    while true do
+      if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+        luasnip.unlink_current()
+      else
+        break
+      end
+    end
+  end,
+})
--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/luasnip.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/luasnip.lua
@@ -1,16 +1,17 @@
+-- see also luasnip-related stuff in config/autocmds.lua
 return {
-	"L3MON4D3/LuaSnip",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
-			require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
-		end,
-	},
+  "L3MON4D3/LuaSnip",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
+      require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
+    end,
+  },
   opts = {
     history = true,
     delete_check_events = "TextChanged",
-    update_events = 'InsertLeave,TextChangedI',
+    update_events = "InsertLeave,TextChangedI",
   },
 }
