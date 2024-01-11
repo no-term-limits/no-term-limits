3ac324dfeeff67b01fc3558f28c019091d00e15b
burnettk <burnettk@users.noreply.github.com>
2024-01-11 18:04:28 -0500

    function add import for term under cursor in python

--- dotfiles_no_term_limits/config/LazyVim/lua/config/keymaps.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/config/keymaps.lua
@@ -21,3 +21,42 @@ end, { noremap = false, silent = false, desc = "Grep word under cursor" })
 
 -- there is never a time when we want to save a visual selection: https://vi.stackexchange.com/a/16735
 vim.cmd([[cabbrev <expr> w getcmdtype()==':' && getcmdline() == "'<,'>w" ? '<c-u>w' : 'w']])
+
+--
+function AddImportForTermUnderCursor()
+  local fullFilePath = vim.fn.expand("%:p")
+  local wordUnderCursor = vim.fn.expand("<cword>")
+
+  local importExpressionToSearchFor = "import " .. wordUnderCursor
+
+  -- Look in other files in the current project and find the import for the word under the cursor
+  if vim.fn.search(importExpressionToSearchFor, "nw") == 0 then
+    -- If the import isn't already there, add it
+    -- vim.print(fullFilePath .. wordUnderCursor)
+    local desiredImportExpression =
+      vim.fn.trim(vim.fn.system("find_import_lines_for_term " .. fullFilePath .. " " .. wordUnderCursor))
+    -- vim.print(desiredImportExpression)
+    vim.print(vim.v.shell_error)
+    if vim.v.shell_error > 0 then
+      vim.cmd('echom "' .. desiredImportExpression .. '"')
+    else
+      -- add the string of text in desiredImportExpression to the top of the file using pure lua stuff without moving the cursor
+
+      -- Get the current buffer content
+      local current_content = vim.fn.getline(1, "$")
+
+      -- Insert the Lua string at the beginning of the table
+      table.insert(current_content, 1, desiredImportExpression)
+
+      -- Set the modified content back to the buffer
+      vim.fn.setline(1, current_content)
+
+      -- Since an import line was added, go back to the place we were by going down one line, so that the cursor is again on top of the same character of the same term
+      vim.cmd("normal j")
+    end
+  else
+    vim.cmd('echom "import already exists"')
+  end
+end
+
+vim.keymap.set("n", "<leader>ri", AddImportForTermUnderCursor, { noremap = false, silent = false, desc = "Add import" })
