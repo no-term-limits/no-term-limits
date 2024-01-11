9c2c877c33f25902d86eadf90d038a28aa5078a3
jasquat <jasquat@users.noreply.github.com>
2024-01-11 10:52:47 -0500

    fixed patterns for quote and bracket for mini.pairs w/ burnettk

--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/mini.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/mini.lua
@@ -2,15 +2,18 @@ return {
   "echasnovski/mini.pairs",
   opts = {
     mappings = {
-      ["("] = { action = "open", pair = "()", neigh_pattern = ".[^%a()]" },
-      ["{"] = { action = "open", pair = "{}", neigh_pattern = ".[^%a{}]" },
-      ['"'] = { action = "closeopen", pair = '""', neigh_pattern = '.[^%a"]', register = { cr = false } },
-      ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '.[^%a`]', register = { cr = false } },
-
-      -- we cannot get the pattern to match [ so just disable it
-      ["["] = false,
-      -- we need a pattern that can match surrounding for works like it's but we do not know how to do that
-      ["'"] = false,
+      -- neigh_pattern, according to the "docs," is a two "character"
+      -- pattern used to decide if it should insert the closing character.
+      -- so for single quote, for example, when we say [^%w'][^%w'],
+      -- that means if there is not a [^%w'] before it (a word character)
+      -- or after it, insert a closing single quote.
+      -- In order to escape certain characters like [ use % instead of \.
+      ["("] = { action = "open", pair = "()", neigh_pattern = ".[^%w()]" },
+      ["{"] = { action = "open", pair = "{}", neigh_pattern = ".[^%w{}]" },
+      ['"'] = { action = "closeopen", pair = '""', neigh_pattern = '.[^%w"]', register = { cr = false } },
+      ["`"] = { action = "closeopen", pair = "``", neigh_pattern = ".[^%w`]", register = { cr = false } },
+      ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%w'][^%w']", register = { cr = false } },
+      ["["] = { action = "open", pair = "[]", neigh_pattern = ".[^%w%[%]]", register = { cr = false } },
     },
   },
 }
