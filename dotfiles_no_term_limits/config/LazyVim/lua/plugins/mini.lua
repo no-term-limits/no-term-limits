9aa4046d5283d04702605214212d3534433edd8a
jasquat <jasquat@users.noreply.github.com>
2024-01-19 16:30:28 -0500

    disable mini pair w/ burnettk

--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/mini.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/mini.lua
@@ -1,19 +1,20 @@
 return {
   "echasnovski/mini.pairs",
-  opts = {
-    mappings = {
-      -- neigh_pattern, according to the "docs," is a two "character"
-      -- pattern used to decide if it should insert the closing character.
-      -- so for single quote, for example, when we say [^%w'][^%w'],
-      -- that means if there is not a [^%w'] before it (a word character)
-      -- or after it, insert a closing single quote.
-      -- In order to escape certain characters like [ use % instead of \.
-      ["("] = { action = "open", pair = "()", neigh_pattern = ".[^%w()]" },
-      ["{"] = { action = "open", pair = "{}", neigh_pattern = ".[^%w{}]" },
-      ['"'] = { action = "closeopen", pair = '""', neigh_pattern = '.[^%w"]', register = { cr = false } },
-      ["`"] = { action = "closeopen", pair = "``", neigh_pattern = ".[^%w`]", register = { cr = false } },
-      ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%w'][^%w']", register = { cr = false } },
-      ["["] = { action = "open", pair = "[]", neigh_pattern = ".[^%w%[%]]", register = { cr = false } },
-    },
-  },
+  enabled = false,
+  -- opts = {
+  --   mappings = {
+  --     -- neigh_pattern, according to the "docs," is a two "character"
+  --     -- pattern used to decide if it should insert the closing character.
+  --     -- so for single quote, for example, when we say [^%w'][^%w'],
+  --     -- that means if there is not a [^%w'] before it (a word character)
+  --     -- or after it, insert a closing single quote.
+  --     -- In order to escape certain characters like [ use % instead of \.
+  --     ["("] = { action = "open", pair = "()", neigh_pattern = ".[^%w()]" },
+  --     ["{"] = { action = "open", pair = "{}", neigh_pattern = ".[^%w{}]" },
+  --     ['"'] = { action = "closeopen", pair = '""', neigh_pattern = '.[^%w"]', register = { cr = false } },
+  --     ["`"] = { action = "closeopen", pair = "``", neigh_pattern = ".[^%w`]", register = { cr = false } },
+  --     ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%w'][^%w']", register = { cr = false } },
+  --     ["["] = { action = "open", pair = "[]", neigh_pattern = ".[^%w%[%]]", register = { cr = false } },
+  --   },
+  -- },
 }
