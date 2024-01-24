5ef10c330721236264127243f40792c23622822a
burnettk <burnettk@users.noreply.github.com>
2024-01-24 15:08:34 -0500

    unhose real time snippet feedback

--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/luasnip.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/luasnip.lua
@@ -3,6 +3,8 @@ return {
   "L3MON4D3/LuaSnip",
   config = function()
     require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
+    -- the load is needed for the tabstops to load in real time
+    require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/snippets/" })
   end,
   opts = {
     history = true,
