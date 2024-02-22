53d815285a7d42747adb4e3b1e8e8edd9961d960
jasquat <jasquat@users.noreply.github.com>
2024-02-22 13:19:01 -0500

    the u bash snippet is working again w/ burnettk

--- dotfiles_no_term_limits/config/LazyVim/snippets/sh.lua
+++ dotfiles_no_term_limits/config/LazyVim/snippets/sh.lua
@@ -1,12 +1,15 @@
-function debug_logger(message)
-  vim.notify(message, "info", { timeout = 60000 })
+function debuglogger(message)
+  local m = "nil"
+  if m then
+    m = tostring(message)
+  end
+  vim.notify(m, "info", { timeout = 60000 })
 end
 
 return {
   s(
     "h",
     f(function(_, snip)
-      -- return vim.fn.trim(vim.fn.system("bash_script_header"))
       local output = vim.fn.systemlist("bash_script_header")
       local trimmed_output = {}
       for _, line in ipairs(output) do
@@ -15,51 +18,42 @@ return {
       return trimmed_output
     end, {})
   ),
-  -- s(
-  --   "u",
-  --   f(function(_, snip)
-  --     -- local regexForVariableAssignments = '^(w+)=?"?%${?(d)(:-w*)?}?"?'
-  --     -- let regexForVariableAssignments = '\v^(\w+)\="?\$\{?(\d)(:-\w*)?\}?"?'
-  --     local regexForVariableAssignments = '^(%w+)="?${(%d)'
-  --     local argVariableAssignmentLineNumbers = {}
-  --     for lineNumber, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, false)) do
-  --       -- print(lineNumber)
-  --       -- print("HEY")
-  --       -- print(line)
-  --       local match, match1 = string.match(line, regexForVariableAssignments)
-  --       debug_logger(match)
-  --       -- debug_logger(match1)
-  --       if match then
-  --         table.insert(argVariableAssignmentLineNumbers, lineNumber + 1)
-  --       end
-  --     end
-  --
-  --     local biggestArgumentNumber = 0
-  --     local variableNames = {}
-  --     for _, lineNumber in ipairs(argVariableAssignmentLineNumbers) do
-  --       local textOnLine = vim.api.nvim_buf_get_lines(0, lineNumber - 1, lineNumber, false)[1]
-  --       local _, _, variableName, variableNumber = string.find(textOnLine, regexForVariableAssignments)
-  --       variableNumber = tonumber(variableNumber)
-  --
-  --       if variableNumber > biggestArgumentNumber then
-  --         biggestArgumentNumber = variableNumber
-  --       end
-  --
-  --       table.insert(variableNames, variableName)
-  --     end
-  --
-  --     local usageLine = '  >&2 echo "usage: $(basename "$0")'
-  --     for _, variableName in ipairs(variableNames) do
-  --       usageLine = usageLine .. " [" .. variableName .. "]"
-  --     end
-  --     usageLine = usageLine .. '"'
-  --
-  --     local firstLine = 'if [[ -z "${' .. biggestArgumentNumber .. ':-}" ]]; then'
-  --     local lines = { firstLine, usageLine, "  exit 1", "fi" }
-  --
-  --     -- vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
-  --
-  --     return lines
-  --   end, {})
-  -- ),
+  s(
+    "u",
+    f(function(_, snip)
+      local regexForVariableAssignments = '^([%w_]+)="?%$%{?(%d)(:-[%w_]*)}?"?'
+      local argVariableAssignmentLineNumbers = {}
+      for lineNumber, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, false)) do
+        local match = string.match(line, regexForVariableAssignments)
+        if match then
+          table.insert(argVariableAssignmentLineNumbers, lineNumber)
+        end
+      end
+
+      local biggestArgumentNumber = 0
+      local variableNames = {}
+      for _, lineNumber in ipairs(argVariableAssignmentLineNumbers) do
+        local textOnLine = vim.api.nvim_buf_get_lines(0, lineNumber - 1, lineNumber, false)[1]
+        local _, _, variableName, variableNumber = string.find(textOnLine, regexForVariableAssignments)
+        variableNumber = tonumber(variableNumber)
+
+        if variableNumber > biggestArgumentNumber then
+          biggestArgumentNumber = variableNumber
+        end
+
+        table.insert(variableNames, variableName)
+      end
+
+      local usageLine = '  >&2 echo "usage: $(basename "$0")'
+      for _, variableName in ipairs(variableNames) do
+        usageLine = usageLine .. " [" .. variableName .. "]"
+      end
+      usageLine = usageLine .. '"'
+
+      local firstLine = 'if [[ -z "${' .. biggestArgumentNumber .. ':-}" ]]; then'
+      local lines = { firstLine, usageLine, "  exit 1", "fi" }
+
+      return lines
+    end, {})
+  ),
 }
