d1c7568a5942a8a84e12741cdcf3655b064ce514
burnettk <burnettk@users.noreply.github.com>
2024-03-08 17:34:32 -0500

    add useState snippet

--- dotfiles_no_term_limits/config/LazyVim/snippets/typescriptreact.lua
+++ dotfiles_no_term_limits/config/LazyVim/snippets/typescriptreact.lua
@@ -1,30 +1,47 @@
 local fmt = require("luasnip.extras.fmt").fmt
 
-local function awesome_func(args, snip)
-	vim.print(args)
-	return args[1][1]
+local function getUseStateSetter(args, _snip)
+  local firstValue = args[1][1]
+  -- vim.print(firstValue)
+  local firstLetter = firstValue:sub(1, 1)
+  local capitalizedFirstLetter = string.upper(firstLetter)
+  local restOfWord = firstValue:sub(2)
+  return "set" .. capitalizedFirstLetter .. restOfWord
 end
 
-return {
-	s("trig", t("also loaded!!")),
-	s("cl", {
-		t('console.log("'),
-		i(1, "value"),
-		t('", '),
-		extras.rep(1),
-		t(")"),
-	}),
-	s(
-		"cl2",
-		fmt(
-			[[
-  console.log("{}", {})
+local console_log_snippet = {
+  t('console.log("'),
+  i(1, "value"),
+  t('", '),
+  extras.rep(1),
+  t(")"),
+}
+
+-- this version does not have a type hint
+-- local use_state_snippet = {
+--   t("const ["),
+--   i(1, "dataVar"),
+--   t(", "),
+--   f(getUseStateSetter, { 1 }),
+--   t("] = useState("),
+--   i(2, "defaultValue"),
+--   t(")"),
+-- }
+
+local awesome_use_state_snippet_with_fmt = fmt(
+  [[
+  const [{}, {}] = useState<{}>({})
   ]],
-			{
-				-- i(1) is at nodes[1], i(2) at nodes[2].
-				i(1, "value"),
-				f(awesome_func, { 1 }),
-			}
-		)
-	),
+  {
+    i(1, "dataVar"),
+    f(getUseStateSetter, { 1 }),
+    i(2, "typeHint"),
+    i(3, "defaultValue"),
+  }
+)
+
+return {
+  s("trig", t("also loaded!!")),
+  s("cl", console_log_snippet),
+  s("us", awesome_use_state_snippet_with_fmt),
 }
