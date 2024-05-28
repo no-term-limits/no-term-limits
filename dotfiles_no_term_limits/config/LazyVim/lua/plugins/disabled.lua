e8afcc432edb20deadfa1bb8f1131d092c6c0ba3
burnettk <burnettk@users.noreply.github.com>
2024-05-28 10:50:04 -0400

    enable flash

--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/disabled.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/disabled.lua
@@ -1,6 +1,17 @@
 return {
-  -- disable flash, the thing "enhances" search by putting letters at the end of each match to let you jump to them
-  { "folke/flash.nvim", enabled = false },
+  -- this is the hammer, to disable flash, the thing enhances search by putting letters at the end of each match to let you jump to them
+  -- we don't actually like coopting slash like this, so we disable it, but it's possible we also can't tolerate the overrides for f, etc.
+  -- { "folke/flash.nvim", enabled = false },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+      },
+    },
+  },
   -- we have our own, very small, snippet collection
   { "rafamadriz/friendly-snippets", enabled = false },
 
