77015a195be656eaf0e277709d1aaad397acc55d
burnettk <burnettk@users.noreply.github.com>
2024-06-14 15:25:14 -0400

    luasnip supertab

--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/nvim-cmp.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/nvim-cmp.lua
@@ -1,70 +1,51 @@
 return {
 
-	"hrsh7th/nvim-cmp",
+  "hrsh7th/nvim-cmp",
 
-	-- config = function()
-	-- 	-- vim.opt.completeopt = { "menu", "menuone", "noselect" }
-	-- 	vim.opt.completeopt = "menu,menuone,noselect"
-	--
-	opts = function(_, opts)
-		local cmp = require("cmp")
-		opts.preselect = cmp.PreselectMode.None
-		opts.completion = { completeopt = "menu,menuone,noselect" }
-		opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
-	end,
-	--  opts = function()
-	--    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-	-- -- end,
-	-- 	local cmp = require("cmp")
-	-- 	local defaults = require("cmp.config.default")()
-	-- 	return {
-	-- 		completion = {
-	-- 			completeopt = "menu,menuone,noinsert",
-	-- 		},
-	-- 		snippet = {
-	-- 			expand = function(args)
-	-- 				require("luasnip").lsp_expand(args.body)
-	-- 			end,
-	-- 		},
-	-- 		mapping = cmp.mapping.preset.insert({
-	-- 			["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-	-- 			["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-	-- 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-	-- 			["<C-f>"] = cmp.mapping.scroll_docs(4),
-	-- 			["<C-Space>"] = cmp.mapping.complete(),
-	-- 			["<C-e>"] = cmp.mapping.abort(),
-	-- 			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	-- 			["<S-CR>"] = cmp.mapping.confirm({
-	-- 				behavior = cmp.ConfirmBehavior.Replace,
-	-- 				select = true,
-	-- 			}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	-- 			["<C-CR>"] = function(fallback)
-	-- 				cmp.abort()
-	-- 				fallback()
-	-- 			end,
-	-- 		}),
-	-- 		sources = cmp.config.sources({
-	-- 			{ name = "nvim_lsp" },
-	-- 			{ name = "luasnip" },
-	-- 			{ name = "path" },
-	-- 		}, {
-	-- 			{ name = "buffer" },
-	-- 		}),
-	-- 		formatting = {
-	-- 			format = function(_, item)
-	-- 				local icons = require("lazyvim.config").icons.kinds
-	-- 				if icons[item.kind] then
-	-- 					item.kind = icons[item.kind] .. item.kind
-	-- 				end
-	-- 				return item
-	-- 			end,
-	-- 		},
-	-- 		experimental = {
-	-- 			ghost_text = {
-	-- 				hl_group = "CmpGhostText",
-	-- 			},
-	-- 		},
-	-- 		sorting = defaults.sorting,
-	-- 	}
-	-- end,
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.preselect = cmp.PreselectMode.None
+    opts.completion = { completeopt = "menu,menuone,noselect" }
+    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+
+    local luasnip = require("luasnip")
+    local mapping = {
+      -- Supertab for luasnip snippets
+      ["<CR>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          if luasnip.expandable() then
+            luasnip.expand()
+          else
+            cmp.confirm({
+              select = true,
+            })
+          end
+        else
+          fallback()
+        end
+      end),
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    }
+    -- Merge with existing mappings
+    opts.mapping = vim.tbl_extend("force", opts.mapping or {}, mapping)
+  end,
 }
