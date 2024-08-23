eb45886b548b4d9c6ac5230bc3ee6e4862b33d1d
jasquat <jasquat@users.noreply.github.com>
2024-08-23 12:09:36 -0400

    autocmd to make file executable if shebang line w/ burnettk

--- dotfiles_no_term_limits/config/LazyVim/lua/config/autocmds.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/config/autocmds.lua
@@ -40,3 +40,29 @@ vim.api.nvim_set_keymap(
   ":lua print(vim.inspect(require('cmp').get_config().sources))<CR>",
   { noremap = true, silent = true }
 )
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  callback = function()
+    local shebang = vim.api.nvim_buf_get_lines(0, 0, 1, true)[1]
+
+    if not shebang or not shebang:match("^#!.+") then
+      return
+    end
+
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      callback = function(args)
+        local filename = vim.api.nvim_buf_get_name(args.buf)
+
+        local fileinfo = vim.uv.fs_stat(filename)
+
+        if not fileinfo or bit.band(fileinfo.mode - 32768, 0x40) ~= 0 then
+          return
+        end
+
+        vim.uv.fs_chmod(filename, bit.bor(fileinfo.mode, 493))
+      end,
+      once = true,
+    })
+  end,
+  desc = "Mark script files with shebangs as executable on write.",
+})
