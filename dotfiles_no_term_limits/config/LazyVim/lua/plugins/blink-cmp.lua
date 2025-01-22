4bd3d7fc6071b54fcee08503e3976486f5b3ddf0
jasquat <jasquat@users.noreply.github.com>
2025-01-22 14:09:53 -0500

    some blink-cmp configs w/ burnettk

---
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+  opts = {
+    snippets = { preset = "luasnip" },
+    -- ensure you have the `snippets` source (enabled by default)
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+    keymap = {
+      preset = "enter",
+      ["<CR>"] = { "fallback" },
+      ["<Tab>"] = { "select_and_accept" },
+    },
+  },
+}
