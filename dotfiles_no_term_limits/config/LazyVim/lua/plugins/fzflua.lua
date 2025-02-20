e355f22020be2199909e3ca999d5a3531386766e
jasquat <jasquat@users.noreply.github.com>
2025-02-20 16:24:00 -0500

    use leader j for find files to avoid conflicts with lazyvim keymaps w/ burnettk

--- dotfiles_no_term_limits/config/LazyVim/lua/plugins/fzflua.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/plugins/fzflua.lua
@@ -1,6 +1,6 @@
 return {
   "ibhagwan/fzf-lua",
   keys = {
-    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>j", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
   },
 }
