570bf3ce59558ead9b26305bf4d42e0a864fdefb
burnettk <burnettk@users.noreply.github.com>
2026-03-05 07:59:55 -0500

    lazy refactor

--- dotfiles_no_term_limits/config/LazyVim/lua/config/lazy.lua
+++ dotfiles_no_term_limits/config/LazyVim/lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import LazyVim extras in the expected order
+    { import = "config.lazy-extras" },
     -- import/override with your plugins
     { import = "plugins" },
   },
